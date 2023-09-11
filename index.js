bfee19fdffd81ef5b1b02b095947f70c38b68c91
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-09-11 18:45:12 +0100

    Get the input box element

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const buttonElements = document.getElementsByTagName('button')
+const inputElements = document.getElementsByTagName('input')
 
 if (buttonElements.length !== 1) {
   throw new Error('Only one button allowed')
 }
 
+if (inputElements.length !== 1) {
+  throw new Error('Only one input box allowed')
+}
+
 const buttonElement = buttonElements[0]
+const inputElement = inputElements[0]
 
 buttonElement.addEventListener('click', () => {
   console.log('Button clicked')
