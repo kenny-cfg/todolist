66e14267c6d749877bbcdd61c024cda31753cde3
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-09-11 18:57:08 +0100

    Add new item to list

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const buttonElements = document.getElementsByTagName('button')
 const inputElements = document.getElementsByTagName('input')
+const listContainerElements = document.getElementsByTagName('ul')
 
 if (buttonElements.length !== 1) {
   throw new Error('Only one button allowed')
@@ -9,10 +10,17 @@ if (inputElements.length !== 1) {
   throw new Error('Only one input box allowed')
 }
 
+if (listContainerElements.length !== 1) {
+  throw new Error('Only one list container allowed')
+}
+
 const buttonElement = buttonElements[0]
 const inputElement = inputElements[0]
+const listContainerElement = listContainerElements[0]
 
 buttonElement.addEventListener('click', () => {
   const newTodoItem = inputElement.value
-  console.log('Button clicked, input is: ', newTodoItem)
+  const newListItemElement = document.createElement('li')
+  newListItemElement.innerText = newTodoItem
+  listContainerElement.append(newListItemElement)
 })
